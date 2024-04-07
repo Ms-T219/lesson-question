73ec21778d5227db1b9d9062e655d64df09e0d7e
miu <155796171+Ms-T219@users.noreply.github.com>
2024-04-07 22:08:05 +0900

    modalのエラー確認

---
+++ trial2/jQuery/script.js
@@ -0,0 +1,62 @@
+$(function(){
+const backBtn =$('#back-btn');
+
+$(window).on('scroll',function(){
+  var scroll =$(this).scrollTop();
+  console.log(scroll);
+
+  if(scroll >=100){
+    backBtn.css('display','inline');
+  }
+  else{
+    backBtn.css('display','none');
+  }
+});
+
+  backBtn.on('click',function(){
+    $('body,html').animate({
+      scrollTop:0
+    },300);
+    return false;
+    })
+
+   // slick スライド
+  $('.autoplay').slick({
+    autoplay:true,
+    dots:true,
+    infinite: true,
+    autoplaySpeed:3000,
+    arrows: false,
+   });
+  
+  const navAbout=$('#nav-about')
+   $('#navAbout').on('click',function(){
+    $('#navAbout').css('color','red');
+   });
+
+  // セクションのフェードイン
+    $(window).on('scroll',function(){
+     const scrollAmount = $(window).scrollTop();
+     const windowHeight = $(window).height();
+    $('section').each(function(){
+     const position =$(this).offset().top;
+     if(scrollAmount > position - windowHeight + 100){
+      $(this).addClass('fade-in');
+     }
+      });
+    });
+ 
+    // モーダル
+    $('.photos-works img').click(function(){
+      const imgSrc = $(this).attr('src');
+      $('.modal-photo').attr('src',imgSrc);
+      $('modal').fadeIn();
+      return false
+    });
+    
+    $('.close-btn').click(function(){
+      $('.modal').fadeOut();
+      return false
+    });
+
+  });
